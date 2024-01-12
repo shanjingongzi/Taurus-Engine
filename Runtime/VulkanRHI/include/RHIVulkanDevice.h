850b44b6d15afee8c045fa2b37e9c82de2f7a7aa
山今公子 <31444825+shanjingongzi@users.noreply.github.com>
2024-01-12 23:24:57 +0800

    modified vulkanviewport

--- Runtime/VulkanRHI/include/RHIVulkanDevice.h
+++ Runtime/VulkanRHI/include/RHIVulkanDevice.h
@@ -45,4 +45,4 @@ namespace
         VulkanPhysicalDeviceFeatures physicalFeatures;
         Array<VkPhysicalDeviceFragmentShadingRateKHR>framentShaderingRates;
     };
-};
+}
--- Runtime/VulkanRHI/include/VulkanViewport.h
+++ Runtime/VulkanRHI/include/VulkanViewport.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <RHIViewport.h>
 #include "VulkanMemory.h"
+#include <Vector2.hpp>
 class VulkanViewport:public RHIViewport,public VulkanRHI::DeviceChild
 {
 public:
@@ -11,6 +12,46 @@ public:
     
     Texture2DRHIRef GetBackBuffer(RHICommandListImmediate &rhicmdList);
     void AdvanceBackBufferFrame(RHICommandLisatImmediate & rhiCmdList);
+
+    virtual void WaitForFrameEventCompletion();
+    virtual void IssueFrameEvent();
+    virtual Vector2 GetSizeXY(){
+        return {sizeX,sizeY}; 
+    }
+    virtual void SetCustomPresent(RHICustomPresent *customPresent);
+    virtual void RHICustomPresent()const override final{
+        return customPresent;
+    }
+    virtual void Tick(float deltaTime)override final;
+    bool Present(VulkanCommandListContext*context,VulkanCmdBuffer*cmdBuffert,VulkanQueue *queue,vulkanQueue*presentQueue,bool lockToVsync);
+    inline uint32 GetPresentCount()const{
+        return presentCount;
+    }
+    inline bool IsFullScreen()const{
+        return isFullScreen;
+    }
+
+    inline VkImage GetBackBufferImage(uint32 index){
+        if(backBufferImages.num()>0){
+            return backBufferImages[index];
+        }
+        else{
+            return VK_NULL_HANDLE;
+        }
+    }
+
+    inline VulkanSwapChain*GetSwapChain(){
+
+    }
+
+    VkSurfaceTransformFlagBtsKHR GetSwapchainQCOMRenderPassTransform()const;
+    VkFormat GetSwapchainImageFormat()const;
+protected:
+    Array<VkImage>backBufferImage;
+    Array<VulkanRHI::Semaphore>RenderingBoneSemaphores;
+    Array<VulkanView>textureViews;
+    RefCountPtr<VulkanBackBuffer>rhiBackBuffer;
+    
 private:
     uint32 index;
 };
